38926768d5f976ccdedd70731a4624e4a66b6d5c
arisnew <aris.new2011@gmail.com>
2016-06-12 14:11:42 +0700

    pertemuan 4

---
+++ myscript.js
@@ -0,0 +1,101 @@
+window.onload = function () {
+	/*
+	//alert('Document ready!');
+	//variable
+	var data1 = 123;	//integer
+	//alert(data1);
+
+	data1 = 'Run';	//string
+	//alert(data1);
+
+	//operator aritmetika
+	//+, -, *, /, %, ++, --
+
+	//<, <=, >, >=, ==, ===, !=, <>,
+	// &&, ||, 
+
+	var a = 10;
+	var b = 15;
+
+	var c = a * b;
+
+	//alert(c);
+
+	var panjang = 29;	//number
+	var nama	= "Teguh";	//string
+	//var cars  	= ["Avanza", "BMW", "Jazz"];	//array
+	var xx 		= {nama:"Agus", umur:23};	//object -> JSON
+
+	function penjumlahan(d,f) {
+		return (d + f);
+	}
+
+	function perkalian(d,f) {
+		alert (d * f);
+	}
+
+	//alert (penjumlahan(100,5));
+	//perkalian(20,8);
+
+
+	//decision
+	//if
+	if (panjang == 21) {
+		//alert ('Panjang = ' + panjang);
+	} else if(panjang > 21){
+		//alert ('Panjang lebih dari 21 ');
+	} else {
+		//alert('Panjang bukan 21!');
+	}
+
+	//switch
+	switch(panjang) {
+		case 20:
+			alert('Panjang 20!');
+			break;
+		case 21:
+			alert('Panjang 21!');
+			break;
+		default:
+			//alert('Panjang unknow');
+			break;
+	}
+
+	//looping
+	
+	for (var i = 1; i <= 100; i++) {
+		document.write(i);
+	}
+
+	document.write("<hr>");
+
+	var x = 1;
+	while(x <= 50){
+		document.write(x);
+		x++;
+	}
+
+	
+	var z  = 100;
+	do {
+		document.write(z);
+		z--;
+	} while (z > 100);
+	
+	//array
+	var cars  	= ["Avanza", "BMW", "Jazz"];	//array
+	var teman = [
+					["agus","Laki-laki","Programmer"],
+					["joko","Laki-laki","Designer"],
+					["ali","Laki-laki","Analist"]
+				]
+	//alert(cars[1]);
+	//alert(teman[1][2]);	//designer -> 2,3
+	for (var i = 0; i < cars.length; i++) {
+		document.write(cars[i]);
+		document.write("<br>");
+	}
+
+	*/
+	//jquery
+}
