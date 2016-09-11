9e2d6b1fa71699060d77971bd1cdf6d4526329ec
arisnew <aris.new2011@gmail.com>
2016-09-11 13:58:28 +0700

    Update data guru

--- guru/proses_edit_guru.php
+++ guru/proses_edit_guru.php
@@ -1,7 +1,12 @@
 <?php
 ob_start();
 include_once '../koneksi.php';
-
-mysql_query("UPDATE data_guru SET id_guru = '$POST[id_guru]',nama = '$POST[nama]',matapelajaran = '$POST[matapelajaran]',email = '$POST[email]'");
-header('location:tampil_guru.php');
+$query = "UPDATE guru SET nama = '$_POST[nama]',matapelajaran = '$_POST[matapelajaran]',email = '$_POST[email]' WHERE id_guru = '$_POST[id_guru]'";
+$result = mysql_query($query);
+if ($result == TRUE) {
+	//header('location:tampil_guru.php');
+	echo '<script>alert("Edit sukses"); window.location ="tampil_guru.php"</script>';
+} else {
+	echo '<script>alert("Edit gagal"); window.location ="tampil_guru.php"</script>';
+}
 ?>
--- guru/tampil_guru.php
+++ guru/tampil_guru.php
@@ -9,18 +9,22 @@
 			<tr><th>No</th><th>nama</th><th>mata pelajaran</th><th>email</th><th>Aksi</th></tr>
 <?php
 include_once "../koneksi.php";
-$tampil ="SELECT * FROM data_guru";
+$tampil ="SELECT * FROM guru";
 $hasil = mysql_query($tampil);
 $no = 1;
-while ($row=mysql_fetch_array($hasil));
-{
-	echo "<tr><td> $no </td>";
-	echo "<td> $row[nama] </td>";
-	echo "<td> $row[matapelajaran] </td>";
-	echo "<td> $row[email] </td>";
-	echo "<td> <a href=edit_guru.php?id=$row[id_guru]>Edit</a> | <a href=hapus_guru.php?id=$row[id_guru]>Hapus</a></td></tr>";
+if(mysql_num_rows($hasil) > 0){
+	while ($data = mysql_fetch_array($hasil))
+	{
+		echo "<tr><td> $no </td>";
+		echo "<td>". $data['nama'] ."</td>";
+		echo "<td>". $data['matapelajaran'] ."</td>";
+		echo "<td>". $data['email'] ."</td>";
+		echo "<td> <a href=edit_guru.php?id=".$data['id_guru'].">Edit</a> | <a href='hapus_guru.php?id=".$data['id_guru']."'>Hapus</a></td></tr>";
 
-	$no++;
+		$no++;
+	}
+} else {
+	echo '<tr><td colspan="5">Tidak ada data! <a href ="index.php"> Tambah baru</a></td></tr>';
 }
 echo "</table>";
 ?>
