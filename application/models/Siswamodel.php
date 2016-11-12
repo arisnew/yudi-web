e850bcee41aa7539a6b2e244f5754818d44108a6
yudiSrilaksono <yudi.srilaksono@student.unsika.ac.id>
2016-11-12 13:38:23 +0700

    new

---
+++ application/models/Siswamodel.php
@@ -0,0 +1,42 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Usermodel extends Model {
+
+    public function __construct() {
+        parent::__construct();
+        $this->table = 'siswa';  //choose table
+        $this->isNew = false;
+        //$this->load->library('formatdate');
+    }
+
+    public function getField($inputs = array()) { // set data input for model (mapping db vs form input)
+        $fields = array(
+            'nis' => $inputs['nis-input'],
+            'nama' => $inputs['nama-input'],
+            'alamat' => $inputs['alamat-input'],
+            'kelas' => $inputs['kelas-input']
+            //'password' => $inputs['password-input']
+        );
+
+        //if ($inputs['password-input'] != '') {
+            //$fields['password'] = $inputs['password-input'];
+        }
+        
+        return $fields;
+
+
+    public function getRules() {    //set rule validasi form
+
+        $nis = array(
+            'field' => 'nis-input', 'label' => 'Nis',
+            'rules' => 'trim|required|max_length[5]|numeric'
+        );
+
+        $nama = array(
+            'field' => 'nama-input', 'label' => 'Nama',
+            'rules' => 'trim|required|max_length[50]'
+        );
+
+        return array($nis, $nama);
+    }
+}
--- application/views/template/_default_menu.php
+++ application/views/template/_default_menu.php
@@ -13,7 +13,6 @@
 	<ul class="treeview-menu">
 		<li><a href="#" onclick="loadContent(base_url + 'view/_table_user')"><i class="fa fa-user"></i> User</a></li>
 		<li><a href="#" onclick="loadContent(base_url + 'view/_table_guru')"><i class="fa fa-user-secret"></i> Guru</a></li>
-		<li><a href="#" onclick="loadContent(base_url + 'view/_siswa_form')"><i class="fa fa-users"></i> Siswa</a></li>
 		<li><a href="#" onclick="loadContent(base_url + 'view/_table_siswa')"><i class="fa fa-users"></i> Siswa</a></li>
 
 	</ul>
